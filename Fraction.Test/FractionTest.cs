28e14b842816eafa2a04e0e4344d2ace1883310b
Raphael Gal <raphaelgal@gmail.com>
2017-04-27 19:07:18 -0400

    Add project files.

---
+++ Fraction.Test/FractionTest.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Fraction.Test
+{
+    public class FractionTest
+    {
+        private Fraction _fractionA;
+        private Fraction _fractionB;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fractionA = new Fraction(3, 5);
+            _fractionB = new Fraction(2, 9);
+        }
+
+        [Test]
+        public void FractionConstructor()
+        {
+            var f1 = new Fraction(3, 5);
+            f1.Numerator.Should().Be(3);
+            f1.Denominator.Should().Be(5);
+
+            f1 = new Fraction(3);
+            f1.Numerator.Should().Be(3);
+            f1.Denominator.Should().Be(1);
+
+            var f2 = new Fraction(f1);
+            f2.Numerator.Should().Be(3);
+            f2.Denominator.Should().Be(1);
+        }
+
+        [Test]
+        public void Reduce()
+        {
+            var f = new Fraction(2, 4);
+            f.Reduce();
+            f.Numerator.Should().Be(1);
+            f.Denominator.Should().Be(2);
+        }
+
+        [Test]
+        public void Add()
+        {
+            var fraction = _fractionA + _fractionB;
+            fraction.Numerator.Should().Be(37);
+            fraction.Denominator.Should().Be(45);
+
+            fraction = new Fraction(2, 4) + new Fraction(2, 4);
+            fraction.Numerator.Should().Be(1);
+            fraction.Denominator.Should().Be(1);
+
+            fraction = new Fraction(1, 4) + new Fraction(1, 4);
+            fraction.Numerator.Should().Be(1);
+            fraction.Denominator.Should().Be(2);
+        }
+
+        [Test]
+        public void Minus()
+        {
+            var fraction = _fractionA - _fractionB;
+            fraction.Numerator.Should().Be(17);
+            fraction.Denominator.Should().Be(45);
+
+            fraction = new Fraction(4, 4) - new Fraction(2, 4);
+            fraction.Numerator.Should().Be(1);
+            fraction.Denominator.Should().Be(2);
+
+            fraction = new Fraction(1, 4) - new Fraction(1, 4);
+            fraction.Numerator.Should().Be(0);
+            fraction.Denominator.Should().Be(1);
+        }
+
+        [Test]
+        public void Multiply()
+        {
+            var fraction = _fractionA * _fractionB;
+            fraction.Numerator.Should().Be(2);
+            fraction.Denominator.Should().Be(15);
+        }
+
+        [Test]
+        public void Divide()
+        {
+            var fraction = _fractionA / _fractionB;
+            fraction.Numerator.Should().Be(27);
+            fraction.Denominator.Should().Be(10);
+        }
+
+        [Test]
+        public void CastToDouble()
+        {
+            double d = _fractionA;
+            d.Should().Be(.6);
+
+            d = _fractionB;
+            d.Should().Be(.222222222222222221);
+
+            d = _fractionA + _fractionB;
+            d.Should().Be(.82222222222222222);
+
+            d = _fractionA - _fractionB;
+            d.Should().Be(.37777777777777777);
+
+            d = _fractionA * _fractionB;
+            d.Should().Be(.133333333333333333);
+
+            d = _fractionA / _fractionB;
+            d.Should().Be(2.7);
+        }
+
+        [Test]
+        public void CastToInt()
+        {
+            var i = (int) _fractionA;
+            i.Should().Be(3);
+
+            i = (int) _fractionB;
+            i.Should().Be(2);
+        }
+
+        [Test]
+        public void LessThan()
+        {
+            var b = _fractionA < _fractionB;
+            b.Should().Be(false);
+
+            b = _fractionB < _fractionA;
+            b.Should().Be(true);
+        }
+
+        [Test]
+        public void GreaterThan()
+        {
+            var b = _fractionA > _fractionB;
+            b.Should().Be(true);
+
+            b = _fractionB > _fractionA;
+            b.Should().Be(false);
+        }
+
+        [Test]
+        public void Equals()
+        {
+            var b = _fractionA == _fractionA;
+            b.Should().Be(true);
+
+            b = _fractionA == _fractionB;
+            b.Should().Be(false);
+        }
+
+        [Test]
+        public void NotEquals()
+        {
+            var b = _fractionA != _fractionA;
+            b.Should().Be(false);
+
+            b = _fractionA != _fractionB;
+            b.Should().Be(true);
+        }
+
+        [Test]
+        public void LessThanOrEquals()
+        {
+            var b = _fractionA <= _fractionB;
+            b.Should().Be(false);
+
+            b = _fractionA <= _fractionA;
+            b.Should().Be(true);
+
+            b = _fractionB <= _fractionA;
+            b.Should().Be(true);
+        }
+
+        [Test]
+        public void GreaterThanOrEquals()
+        {
+            var b = _fractionA >= _fractionB;
+            b.Should().Be(true);
+
+            b = _fractionA >= _fractionA;
+            b.Should().Be(true);
+
+            b = _fractionB >= _fractionA;
+            b.Should().Be(false);
+        }
+    }
+}
---
+++ Fraction/Fraction.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    public class Fraction
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentOutOfRangeException($"message", "Denominator may not be 0");
+            }
+
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public Fraction(int numerator) : this(numerator, 1)
+        {
+        }
+
+        public Fraction(Fraction otherFraction) : this(otherFraction.Numerator, otherFraction.Denominator)
+        {
+        }
+
+        public Fraction Reduce()
+        {
+            var gcd = GetGcd(Math.Abs(Numerator), Denominator);
+
+            if (gcd == 1) return this;
+
+            Numerator = Numerator / gcd;
+            Denominator = Denominator / gcd;
+
+            return this;
+        }
+
+
+        private static int GetGcd(int a, int b)
+        {
+            while (true)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+
+                var a1 = a;
+                a = b;
+                b = a1 % b;
+            }
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            var numerator = (f1.Numerator * f2.Denominator) + (f2.Numerator * f1.Denominator);
+            var denominator = f1.Denominator * f2.Denominator;
+
+            return new Fraction(numerator, denominator).Reduce();
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            var numerator = (f1.Numerator * f2.Denominator) - (f2.Numerator * f1.Denominator);
+            var denominator = f1.Denominator * f2.Denominator;
+
+            return new Fraction(numerator, denominator).Reduce();
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            var numerator = f1.Numerator * f2.Numerator;
+            var denominator = f1.Denominator * f2.Denominator;
+
+            return new Fraction(numerator, denominator).Reduce();
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            var numerator = f1.Numerator * f2.Denominator;
+            var denominator = f1.Denominator * f2.Numerator;
+
+            return new Fraction(numerator, denominator).Reduce();
+        }
+
+        public static implicit operator double(Fraction f)
+        {
+            return (double) f.Numerator / f.Denominator;
+        }
+
+        public static explicit operator int(Fraction f)
+        {
+            return f.Numerator;
+        }
+
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            return (double) f1 < f2;
+        }
+
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            return !(f1 < f2);
+        }
+
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+            return f1.Numerator.Equals(f2.Numerator) && f2.Denominator.Equals(f2.Denominator);
+        }
+
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            return !(f1 == f2);
+        }
+
+        public static bool operator <=(Fraction f1, Fraction f2)
+        {
+            return (f1 < f2) || (f1 == f2);
+        }
+
+        public static bool operator >=(Fraction f1, Fraction f2)
+        {
+            return (f1 > f2) || (f1 == f2);
+        }
+    }
+}
